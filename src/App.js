2286d0231e1fe1a9faad0589198b7d537d3e7dd4
DerLippo <32038450+DerLippo@users.noreply.github.com>
2025-08-05 20:35:24 +0200

    funkt

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { View, Button, TextInput } from 'react-native';
+import { View, Button, TextInput, Text } from 'react-native';
 
 import Make_text from './text.js';
-import NFform from './reuse/components.js'
+import { NFform } from './reuse/components.js';
 
 import tempres from './temporary_server_responses.js';
 
@@ -50,91 +50,46 @@ function App() {
   }
 
   switch (SELECTEDPAGE) {
-    case page_e.LANDING_LOGIN: {
+    case page_e.LANDING_REGISTER: {
       const elements = [
-        {'type': 'email', 'name': 'email', 'instruction': 'ENTER YOUR EMAIL'},
-        {'type': 'password', 'name': 'password', 'instruction': 'ENTER YOUR PASSWORD'}
+        { type: 'email', name: 'email', instruction: 'ENTER YOUR EMAIL' },
+        {
+          type: 'password',
+          name: 'password',
+          instruction: 'ENTER YOUR PASSWORD',
+        },
       ];
+
       const callbacks = {
         on_success: () => {},
-        on_return: () => {}
+        on_return: () => {},
       };
 
       return (
         <View style={styles.container}>
-          <NFform 
-            callbacks={callbacks}
-            elements={elements}
-          />
+          <Text>Register</Text>
+          <NFform callbacks={callbacks} elements={elements} />
         </View>
-      )
-
-
-
-      // return (
-      //   <View style={styles.container}>
-      //     <View style={formStyles.form_border}>
-      //       <Make_text
-      //         args={formStyles.form_instruction_text}
-      //         string={'EMAIL:'}
-      //       />
-      //       <TextInput
-      //         style={formStyles.input_border}
-      //         onChangeText={text => CHANGE_EMAILTEXT(text)}
-      //         value={EMAIL_TEXT}
-      //         textContentType="email"
-      //         autoComplete="email"
-      //       />
-      //       <Make_text
-      //         args={formStyles.form_instruction_text}
-      //         string={'PASSWORD:'}
-      //       />
-      //       <TextInput
-      //         style={formStyles.input_border}
-      //         onChangeText={text => CHANGE_PASSWORDTEXT(text)}
-      //         value={PASSWORD_TEXT}
-      //         textContentType="password"
-      //         autoComplete="current-password"
-      //         secureTextEntry
-      //       />
-      //       <Button title={'SUBMIT'} onPress={() => check_login_data()} />
-      //     </View>
-      //     <Button
-      //       title={'REGISTER'}
-      //       onPress={() => SET_SELECTEDPAGE(page_e.LANDING_REGISTER)}
-      //     />
-      //   </View>
-      // );
+      );
     }
-    case page_e.LANDING_REGISTER: {
+    case page_e.LANDING_LOGIN: {
+      const elements = [
+        { type: 'email', name: 'email', instruction: 'ENTER YOUR EMAIL' },
+        {
+          type: 'password',
+          name: 'password',
+          instruction: 'ENTER YOUR PASSWORD',
+        },
+      ];
+      const callbacks = {
+        on_success: () => {},
+        on_return: () => {},
+      };
+
       return (
         <View style={styles.container}>
-          <View style={formStyles.form_border}>
-            <Make_text
-              args={formStyles.form_instruction_text}
-              string={'WHAT IS YOUR EMAIL:'}
-            />
-            <TextInput
-              style={formStyles.input_border}
-              onChangeText={text => CHANGE_EMAILTEXT(text)}
-              value={EMAIL_TEXT}
-              textContentType="email"
-              autoComplete="email"
-            />
-            <Make_text
-              args={formStyles.form_instruction_text}
-              string={'WHAT IS YOUR PASSWORD:'}
-            />
-            <TextInput
-              style={formStyles.input_border}
-              onChangeText={text => CHANGE_PASSWORDTEXT(text)}
-              value={PASSWORD_TEXT}
-              textContentType="password"
-              autoComplete="current-password"
-              secureTextEntry
-            />
-            <Button title={'SUBMIT'} onPress={() => check_register_data()} />
-          </View>
+          <Text>Login</Text>
+          <NFform callbacks={callbacks} elements={elements} />
         </View>
       );
     }
@@ -146,66 +101,6 @@ function App() {
         />
       );
   }
-
-  /*
-😂
-<TextInput
-  editable
-  multiline
-  numberOfLines={4}
-  maxLength={40}
-  onChangeText={text => CHANGE_EMAILTEXT(text)}
-  value={EMAIL_TEXT}
-  style={{
-    borderWidth: 1,
-    borderColor: 'black',
-    backgroundColor: 'white',
-    borderRadius: 8,
-    minHeight: 40,
-    padding: 8,
-    marginTop: 16,
-    color: 'black'
-  }}
-/>
-😂😂😂
-<TextInput
-  textContentType="username"
-  autoComplete="username"
-  autoCapitalize="none"
-/>
-<TextInput
-  textContentType="password"
-  autoComplete="password"
-  secureTextEntry
-/>
-            */
-
-  // switch (SELECTEDPAGE) {
-  //   case page_e.MAINPAGE:
-  //     new_h1_text['backgroundColor'] = '#ff0000';
-
-  //     return (
-  //       <View style={styles.container}>
-  //         <Make_text args={new_h1_text} string={'hello MAINPAGE'} />
-  //         <Button
-  //           title={'Click me'}
-  //           onPress={() => SET_SELECTEDPAGE(PAGE.STARTINGPAGE)}
-  //         />
-  //       </View>
-  //     );
-  //   default:
-  //     new_h1_text['backgroundColor'] = 'green';
-
-  //     return (
-  //       <View style={styles.container}>
-  //         <Make_text args={new_h1_text} string={'hello STARTINGPAGE'} />
-  //         <Button
-  //           title={'Click me'}
-  //           onPress={() => SET_SELECTEDPAGE(PAGE.MAINPAGE)}
-  //         />
-  //       </View>
-  //     );
-  // }
 }
 
 export default App;
--- src/reuse/components.js
+++ src/reuse/components.js
@@ -1,58 +1,54 @@
-import { useState } from "react";
-import { View, Button, TextInput } from 'react-native';
-import { formStyles } from "./styles";
+import { useState } from 'react';
+import { View, Button, TextInput, Text } from 'react-native';
+import { formStyles } from './styles';
 
 const NFform = ({ callbacks, elements }) => {
-
   const [form_data, set_form_data] = useState(() => {
     const init_state = {};
     elements.forEach(element => {
       let init_value = {};
       init_value['value'] = '';
       init_value['type'] = element.type;
-      if(element.type === 'password') init_value['secure'] = true;
+      if (element.type === 'password') init_value['secure'] = true;
       init_state[element.name] = init_value;
-    })
+    });
     return init_state;
   });
-
-  function on_input(field, string){
+  function on_input(field, string) {
     set_form_data(prev => ({
       ...prev,
       [field]: {
         ...prev[field],
-        value: string
-      }
+        value: string,
+      },
     }));
   }
-
   function on_submit() {
     /**
      * TODO server requests
      *      validity checks
      */
-    throw new Error("on_submit not implemented");
+    throw new Error('on_submit not implemented');
   }
-
   return (
     <View style={formStyles.form_border}>
-      {
-        elements.map(elem => (
-          <View>
-            <Text style={formStyles.form_instruction_text}>{elem.instruction}</Text>
-            <TextInput 
-              style={formStyles.input_border} 
-              placeholder={`ENTER ${elem.name}`}
-              value={form_data[elem.name].value}
-              onChangeText={text => on_input(elem.name, text)}
-              {...form_data[elem.name].secure ? secureTextEntry : {}}
-            />
-          </View>
-        ))
-      }
-      <Button title={'SUBMIT'} onPress={() => on_submit()}/>
+      {elements.map((elem, index) => (
+        <View key={index}>
+          <Text style={formStyles.form_instruction_text}>
+            {elem.instruction}
+          </Text>
+          <TextInput
+            style={formStyles.input_border}
+            placeholder={`ENTER ${elem.name}`}
+            value={form_data[elem.name].value}
+            onChangeText={text => on_input(elem.name, text)}
+            secureTextEntry={form_data[elem.name].secure}
+          />
+        </View>
+      ))}
+      <Button title={'SUBMIT'} onPress={() => on_submit()} />
     </View>
-  )
-}
+  );
+};
 
 export { NFform };
